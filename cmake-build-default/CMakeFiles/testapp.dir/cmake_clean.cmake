ab16be16c98db956832b7df784656e56a794df8b
Ege Hatırnaz <egehatirnaz@hotmail.com>
2020-03-30 20:24:54 +0300

    Implementation of Monte Carlo

---
+++ cmake-build-default/CMakeFiles/testapp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testapp.dir/main.c.o"
+  "testapp"
+  "testapp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/testapp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 #include <float.h>
+#include <math.h>
 
 float *MPI_Map_Func(float *arr, int size, float (*func)(float)){
     int rank, comsize;
@@ -13,12 +14,12 @@ float *MPI_Map_Func(float *arr, int size, float (*func)(float)){
     MPI_Comm_size(MPI_COMM_WORLD, &comsize);   // size is no. of processes.
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    float *buff = malloc(sizeof(float) * size + 1);
-    MPI_Scatter(arr, size, MPI_FLOAT, buff,
+    float *buff = calloc(size + 1, sizeof(float));
+    MPI_Scatter(&arr, size, MPI_FLOAT, buff,
                 size, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     float *locals = NULL;
-    locals = malloc(sizeof(float) * size + 1);
+    locals = calloc(size + 1, sizeof(float));
     float local = func(arr[rank]);
     MPI_Allgather(&local, 1, MPI_FLOAT, locals, 1, MPI_FLOAT,
                MPI_COMM_WORLD);
@@ -29,19 +30,41 @@ float *MPI_Map_Func(float *arr, int size, float (*func)(float)){
     return arr;
 }
 
-float* MPI_Filter_Func(float* arr, int size, bool (*pred)(float)){
+float* MPI_Fold_Func(float* arr, int size, float initial_value, float (*func)(float, float)){
     int rank, comsize;
-    float *result = malloc(sizeof(float) * size + 1);
+    float *result = calloc(size + 1, sizeof(float));
 
     MPI_Comm_size(MPI_COMM_WORLD, &comsize);   // size is no. of processes.
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    float *buff = malloc(sizeof(float) * size + 1);
-    MPI_Scatter(arr, size, MPI_FLOAT, buff,
+    float *buff = calloc(size + 1, sizeof(float));
+    MPI_Scatter(&arr, size, MPI_FLOAT, buff,
                 size, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
     float *locals = NULL;
-    locals = malloc(sizeof(float) * size + 1);
+    locals = calloc(size + 1, sizeof(float));
+    float local = func(arr[rank], initial_value);
+    MPI_Allgather(&local, 1, MPI_FLOAT, locals, 1, MPI_FLOAT, MPI_COMM_WORLD);
+
+    for(int i = 0; i < size; i++){
+        result[i] = locals[i];
+    }
+    return result;
+}
+
+float* MPI_Filter_Func(float* arr, int size, bool (*pred)(float)){
+    int rank, comsize;
+    float *result = calloc(size + 1, sizeof(float));
+
+    MPI_Comm_size(MPI_COMM_WORLD, &comsize);   // size is no. of processes.
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    float *buff = calloc(size + 1, sizeof(float));
+    MPI_Scatter(&arr, size, MPI_FLOAT, buff,
+                size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    float *locals = NULL;
+    locals = calloc(size + 1, sizeof(float));
     float local = pred(arr[rank]);
     MPI_Allgather(&local, 1, MPI_FLOAT, locals, 1, MPI_FLOAT, MPI_COMM_WORLD); // Maybe it's a bad idea.
 
@@ -64,21 +87,95 @@ bool is_positive(float a){
     }
 }
 
-int main(int argc, char *argv[]){
-    float *res2, *res1;
-    float arr1[3] = {-1.0f, 1.2f, 1.7f};
-    float arr2[3] = {-1.0f, 1.2f, 1.7f};
-    float (*func_add)(float) = &add;
-    bool (*func_pos)(float) = &is_positive;
-    int size = 3;
+float multiply(float a, float initial_value){
+    return a * initial_value;
+}
 
-    MPI_Init(NULL, NULL);
+float randomfloat(float val){
+    val = (float)arc4random() / UINT32_MAX;
+    return val;
+}
+
+float hitscan(float x, float y){
+    float center_x, center_y;
+    center_x = 0.5f;
+    center_y = 0.5f;
+    if(pow((x - center_x), 2) + pow((y - center_y), 2) <= pow(0.5f, 2)){
+        return 1.0f;
+    } else {
+        return -1.0f;
+    }
+}
+
+float monte_carlo(float* arr_x, int size){
+    int comsize, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &comsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int hit_count = 0;
+    float ratio = 0.0f;
+        
+    float* scan;
+    float initial_value = 0;
+    float* hit_results;
+    
+    float (*func_rand)(float) = &randomfloat;
+    float (*func_hits)(float, float) = &hitscan;
+    bool (*func_pos)(float) = &is_positive;
+    
+    // Use MPI_Map for random initialization.
+    arr_x = MPI_Map_Func(arr_x, size, func_rand);
+    float rand_coord = randomfloat(initial_value);
+    
+    // Use MPI_Fold for combining one coordinate with another random coordinate.
+    scan = MPI_Fold_Func(arr_x, size, rand_coord, func_hits);
+    
+    // Filter if it's a hit.
+    hit_results = MPI_Filter_Func(scan, size, func_pos);
+    
+    for(int i = 0; i < size; i++){
+        if(hit_results[i] == 1.00)
+            hit_count += 1;
+    }
+      
+    return hit_count;
+}
+
+float monte_helper(float* orig_arr, int size){
+    float result = 0.0f;
+    int comsize, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &comsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    
+    float *buff = calloc(comsize + 1, sizeof(float));
+    MPI_Scatter(&orig_arr, size, MPI_FLOAT, buff,
+                size, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    
+    float *locals = NULL;
+    locals = calloc(size + 1, sizeof(float));
+    float local = monte_carlo(buff, size/comsize);
+    MPI_Allgather(&local, 1, MPI_FLOAT, locals, 1, MPI_FLOAT, MPI_COMM_WORLD); // Maybe it's a bad idea.
+
+    for(int i = 0; i < size; i++){
+        result += locals[i];
+    }
+    return result/size;
+}
+
+int main(int argc, char *argv[]){
+    float arr1[100];
+    int size = 100;
+
+    MPI_Init(&argc, &argv);
     int rank, comsize;
     MPI_Comm_size(MPI_COMM_WORLD, &comsize);   // size is no. of processes.
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    res1 = MPI_Filter_Func(arr1, size, func_pos);
-    res2 = MPI_Map_Func(arr2, size, func_add);
+    //res1 = MPI_Map_Func(arr1, size, func_add);
+    //res2 = MPI_Fold_Func(arr2, size, initial_value, func_mult);
+    //res3 = MPI_Filter_Func(arr3, size, func_pos);
+    printf("%f\n", monte_helper(arr1, size));
+    /*
     if(rank == 0) {
         printf("%f\n", res1[0]);
         printf("%f\n", res1[1]);
@@ -87,7 +184,11 @@ int main(int argc, char *argv[]){
         printf("%f\n", res2[0]);
         printf("%f\n", res2[1]);
         printf("%f\n\n", res2[2]);
-    }
+
+        printf("%f\n", res3[0]);
+        printf("%f\n", res3[1]);
+        printf("%f\n\n", res3[2]);
+    }*/
     MPI_Finalize();
 
 }
